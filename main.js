7fdcc984e78e0f6d18ce74af921506ae43ff9e0d
birennicolas <90913609+birennicolas@users.noreply.github.com>
2021-12-08 16:29:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+
+    if (window.scrollY > 120) {
+        nav.style.top = 0;
+    }
+});
