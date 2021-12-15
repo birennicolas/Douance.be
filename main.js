361a6663e59c6bbac012b8b8fed2330985331503
birennicolas <90913609+birennicolas@users.noreply.github.com>
2021-12-15 12:32:43 +0100

    hamburger

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const nav = document.querySelector("nav");
-
-window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
-
-    if (window.scrollY > 120) {
-        nav.style.top = 0;
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
     }
 });
-
