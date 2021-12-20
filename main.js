3f7529694d4c5cc7837ffb677a7e4e35d8bf149a
birennicolas <90913609+birennicolas@users.noreply.github.com>
2021-12-20 18:22:19 +0100

    toggle menu croix assoc

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const navMenu = document.querySelector('.nav-links');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
-        menuBtn.classList.add('open');
+        menuBtn.classList.toggle('open');
         navMenu.classList.toggle('active')
         menuOpen = true;
     } else {
