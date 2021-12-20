897e0479ce66e02c2f75b1d2149b727495aa52b2
birennicolas <90913609+birennicolas@users.noreply.github.com>
2021-12-20 16:34:55 +0100

    responsive-assoc

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-links');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
+        navMenu.classList.toggle('active')
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        navMenu.classList.remove('active');
         menuOpen = false;
     }
 });
+
